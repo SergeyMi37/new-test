b6f3dda71c292e336fda505914c21ae95a4d50b7
SergeyMi37 <sergey.mikhaylenko@gmail.com>
2023-02-09 12:12:58 +0300

    upd

--- check-send.py
+++ check-send.py
@@ -6,9 +6,9 @@ import datetime
 #print(datetime.datetime.today())
 
 def send_telegram(text: str):
-    TOKEN=""
+    TOKEN="111"
     url = "https://api.telegram.org/bot"
-    channel_id = ""
+    channel_id = "555"
     url += TOKEN
     method = url + "/sendMessage"
 
@@ -24,25 +24,25 @@ def send_telegram(text: str):
         print("Послано удачно")
 
 def f():
-    threading.Timer(15.0, f).start()  # Перезапуск через 3600 секунд - каждый час
+    threading.Timer(3600.0, f).start()  # Перезапуск через 3600 секунд - каждый час
     _dt=str(datetime.datetime.today().strftime("%Y-%m-%d_%H.%M"))
-    tfile=open("/sys/bus/w1/devices//w1_slave")
+    tfile=open("/sys/bus/w1/devices/28-3cade381c52f/w1_slave")
     ttext=tfile.read()
     tfile.close()
     temp=ttext.split("\n")[1].split(" ")[9]
     _temp=float(temp[2:])/1000
-    print(_temp)
+    #print(_temp)
     if _temp<15:
-        _msg=_dc+" 🚨 Внимание предельный порог темпратуры ! "+_temp
-		#send_telegram(_msg)
+        _msg="👉"+_dt+" 🚨 Внимание предельный порог темпратуры "+str(_temp)
+        send_telegram(_msg)
         print(_msg)
     else:
-        _msg=_dc+" темпратура "+_temp
+        _msg="👉"+_dt+" темпратура "+str(_temp)
         print(_msg)
-		#send_telegram(_msg)
+        send_telegram(_msg)
 
 if __name__ == '__main__':
-	#send_telegram("Привет из питона!")
-	f()
+    #send_telegram("Привет из питона!")
+    f()
 else:
     print(__name__)
